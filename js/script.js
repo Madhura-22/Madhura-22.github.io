1fe5275c44d0c211ecaa9f3abccf0e0d0e8718f2
nikhil-git25 <u1266557@umail.utah.edu>
2020-11-03 11:32:42 -0700

    nikhil trial

--- js/script.js
+++ js/script.js
@@ -3,14 +3,7 @@ loadData().then(data => {
     
 });
 
-// ******* DATA LOADING *******
-// We took care of that for you
 
-/**
- * A file loading function or CSVs
- * @param file
- * @returns {Promise<T>}
- */
 async function loadFile(file) {
     let data = await d3.csv(file).then(d => {
         let mapped = d.map(g => {
