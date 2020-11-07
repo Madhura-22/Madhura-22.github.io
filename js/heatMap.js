3cf49fd9396b3af89f5f0ca45820b31f75cf66dd
nikhil-git25 <u1266557@umail.utah.edu>
2020-11-07 15:52:56 -0700

    Year scroll and tooltip

--- js/heatMap.js
+++ js/heatMap.js
@@ -21,53 +21,41 @@ class CountryData {
 class Map{
     constructor(data) {
         this.projection = d3.geoCylindricalEqualArea().scale(800,800).translate([-400, 700]);
-        // this.nameArray = data.population.map(d => d.geo.toUpperCase());
         this.suicides = data.suicides;
         this.Total=data.Total
-        console.log(data.Total)
         this.data=data;
+		this.activeYear = 2002;
+		this.draw_Scroll();
      
     }
 
     drawMap(world) {
        
+		
+		let mapchart = d3.select("#map-chart").append("svg");
+		let mapLayer=mapchart.append("g").attr("id","mapLayer");
+		let that=this;
+		
+		let mapBrew =['rgb(128,0,0)','rgb(178,34,34)','rgb(255,0,0)','rgb(255,69,0)','rgb(255,99,71)','rgb(240,128,128)','rgb(255,160,122)'];
+		let mapRange = [12000,10000,8000,6000,4000,2000,0]; 
 
-        console.log("in drawMap");
-        let mapchart = d3.select("#map-chart").append("svg");
-        let mapLayer=mapchart.append("g").attr("id","mapLayer");
-       
-        console.log(world);
-        console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
-        console.log(this.data.Total[0].State);
-       
-       let that=this;
+		let yearTotalData=that.data.Total.filter(d=>d.Year==this.activeYear);
+		
+		// Define the div for the tooltip
+		var div = d3.select("body").append("div")   
+			.attr("class", "tooltip")               
+			.style("opacity", 0);
+		
+        //Drawing the map 
+		let Gpath = d3.geoPath().projection(this.projection);
 
-         let mapBrew =['rgb(128,0,0)','rgb(178,34,34)','rgb(255,0,0)','rgb(255,69,0)','rgb(255,99,71)','rgb(240,128,128)','rgb(255,160,122)'];
-
-        let mapRange = [12000,10000,8000,6000,4000,2000,0]; 
-
-        //console.log(geoJSON);
-         let Gpath = d3.geoPath().projection(this.projection);
-        //console.log(Gpath)
-
-        let yearTotalData=that.data.Total.filter(d=>d.Year=="2001");
-        console.log("#############################")
-        console.log(yearTotalData);
-
-        // Define the div for the tooltip
-        var div = d3.select("body").append("div")   
-            .attr("class", "tooltip")               
-            .style("opacity", 0);
-
-        console.log("****************************");
-        console.log(world.features)
         mapLayer.selectAll("path")
                     .data(world.features)
                     .enter()
                     .append("path")
                     .attr("d", Gpath)
-
-                    .style("fill",function(d){
+					// Coloring for the heatmap
+                    .style("fill",function(d){ 
 
                         for(let i=0;i<34;i++){
 
@@ -86,24 +74,115 @@ class Map{
 
                             }
 
-                        }
-                           })
-
-                      .on("mouseover", function(d) {      
-                            div.transition()        
-                                .duration(200)      
-                                .style("opacity", .9);      
-                            div .html(d.properties.NAME_1)  
-                                .style("left", (d3.event.pageX) + "px")     
-                                .style("top", (d3.event.pageY - 28) + "px");    
-                            })                  
-                        .on("mouseout", function(d) {       
-                            div.transition()        
-                                .duration(500)      
-                                .style("opacity", 0);   
+						}
+                        })
+						   
+					// Hover tooltip
+                    .on("mouseover", function(d) {      
+						div.transition()        
+							.duration(200)      
+							.style("opacity", .9);      
+						div .html(that.tooltipRender(d,yearTotalData))  
+							.style("left", (d3.event.pageX) + "px")     
+							.style("top", (d3.event.pageY - 28) + "px");    
+						})                  
+                    .on("mouseout", function(d) {       
+						div.transition()        
+							.duration(500)      
+							.style("opacity", 0);   
                         });
 
     
+    }
+	
+	updateMap() {
+       
+		
+		let that = this;
+		let mapLayer=d3.selectAll("#mapLayer");
+		
+		let mapBrew =['rgb(128,0,0)','rgb(178,34,34)','rgb(255,0,0)','rgb(255,69,0)','rgb(255,99,71)','rgb(240,128,128)','rgb(255,160,122)'];
+		let mapRange = [12000,10000,8000,6000,4000,2000,0]; 
+
+		let yearTotalData=that.data.Total.filter(d=>d.Year==this.activeYear);
+		
+
+        mapLayer.selectAll("path")
+				.style("fill",function(d){ 
+					for(let i=0;i<34;i++){
+					 if(d.properties.NAME_1==yearTotalData[i].State){
+						
+						var colors=mapBrew,
+							 range = mapRange;
+
+						return  yearTotalData[i].Total > range[0] ? colors[0] :
+								yearTotalData[i].Total > range[1] ? colors[1] :
+								yearTotalData[i].Total > range[2] ? colors[2] :
+								yearTotalData[i].Total > range[3] ? colors[3] :
+								yearTotalData[i].Total> range[4] ? colors[4] :
+								yearTotalData[i].Total > range[5] ? colors[5] :
+								colors[6];
+
+						}
+
+					}
+					})
+						   
+
+    
+    }
+	
+	draw_Scroll()
+	{
+		// Working on the year scroll
+		d3.select('#map-chart')
+            .append('div').attr('id', 'activeYear-bar');
+		
+
+        let that = this;
+
+        //Slider to change the activeYear of the data
+        let yearScale = d3.scaleLinear().domain([2001, 2012]).range([30, 730]);
+
+        let yearSlider = d3.select('#activeYear-bar')
+            .append('div').classed('slider-wrap', true)
+            .append('input').classed('slider', true)
+            .attr('type', 'range')
+            .attr('min', 2001)
+            .attr('max', 2012)
+            .attr('value', this.activeYear);
+
+        let sliderLabel = d3.select('.slider-wrap')
+            .append('div').classed('slider-label', true)
+            .append('svg');
+
+        let sliderText = sliderLabel.append('text').text(this.activeYear);
+
+        sliderText.attr('x', yearScale(this.activeYear));
+        sliderText.attr('y', 25);
+
+        yearSlider.on('input', function () {
+			sliderText.text(this.value);
+            sliderText.attr('x', yearScale(this.value));
+			that.activeYear = this.value;
+			that.updateMap()
+			
+        });
+    
+	}
+	
+	tooltipRender(data,yearTotalData) {
+		let ind;
+		for(let i=0;i<34;i++){
+            if(data.properties.NAME_1==yearTotalData[i].State)
+			{
+				ind = i;
+				break;
+			}
+		}
+                            
+		let text = "<h3>" + data.properties.NAME_1 + "</h3>" + "<p> total:" + yearTotalData[ind].Total + "</p>";
+		return text;
     }
 
 
