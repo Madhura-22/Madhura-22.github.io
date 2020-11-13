8905b3edd703ab6e567669eddf26b5d87ed13992
nikhil-git25 <u1266557@umail.utah.edu>
2020-11-13 13:16:18 -0700

    grouped bar chart

--- js/barChart.js
+++ js/barChart.js
@@ -10,20 +10,15 @@ class Bar{
 
     drawBar() {
        
-		
-
-		// let Gen_data=this.data.Gender.filter(d=>d.Year==this.activeYear);
-		// console.log(Gen_data);
 		console.log("happpy")
-		//console.log(this.data);
-		let width=960
-		let height=500
+		let width=700;
+		let height=400;
 
 		let trialData=this.data.Gender.filter(d=>d.Year==this.activeYear && d.State=="Karnataka");
 		console.log(trialData)
 		
 
-		let barChart = d3.select("#bar-plot").append("svg");
+		let barChart = d3.select("#bar-plot").append("svg").attr("height",500).attr("width",700);
 		let barLayer=barChart.append("g").attr("id","barLayer");
 
 		let categories=[]
@@ -33,38 +28,51 @@ class Bar{
 		}
 		console.log(categories)
 
-		var groups = d3.map(trialData, function(d){return(d.Gender)}).keys()
-		console.log("groups")
-		console.log(groups)
+		var groups = ["Male", "Female"]
 		
 
 
 
 		var x = d3.scaleBand()
-		      .domain(groups)
+		      .domain(categories)
 		      .range([0, width])
-		      .padding([0.2])
+		      .padding([0.2]);
+	
 		barLayer.append("g")
-		    //.attr("transform", "translate(0," + 700 + ")")
+			.attr("transform","translate(0,300)")
 		    .call(d3.axisBottom(x).tickSize(0));
+			
+		let ticklabel = d3.selectAll(".tick").selectAll("text").attr("transform","rotate(90) translate(8,8)").style("text-anchor", "start");;
+		console.log(ticklabel);
+
 
 		  // Add Y axis
 		  var y = d3.scaleLinear()
-		    .domain([0, 40])
-		    .range([ height, 0 ]);
+		    .domain([-50, 2500])
+		    .range([ 0, 300]);
 		  barLayer.append("g")
 		    .call(d3.axisLeft(y));
 
 		
 		  var xSubgroup = d3.scaleBand()
-		    .domain(categories)
-		    .range([0, x.bandwidth()])
+		    .domain(groups)
+		    .range([0, x.bandwidth()-26])
 		    .padding([0.05])
 
 		  
 		  var color = d3.scaleOrdinal()
-		    .domain(categories)
+		    .domain(groups)
 		    .range(['#e41a1c','#377eb8'])
+			
+		   barLayer.selectAll("rect")
+					.data(trialData)
+					.enter().append("rect")
+					  .attr("x", function(d,i) { return xSubgroup(d.Gender)+ x(d.Categories) ; })
+					  .attr("y",function(d){ return 300 - y(d.Total)})
+					  .attr("width", 20)
+					  .attr("height",function(d){ return y(d.Total)})
+					  .attr("fill", function(d) { return color(d.Gender); });
+
 
 		
 	}	
