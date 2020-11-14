321025e450f8339ad717aed4a2b8f31e11e8fc9f
Madhura <u1266039@utah.edu>
2020-11-14 14:32:49 -0700

    cleaned

--- js/barChart.js
+++ js/barChart.js
@@ -228,80 +228,12 @@ class Bar{
 					  .attr("height",function(d){ return y(d.Total)})
 					  .attr("fill", function(d) { return color(d.Age_group); });
 
-
-					 
-
-
 		
 		}
 
 	
 	});
 
-
-		// let trialData=this.data.Gender.filter(d=>d.Year==activeYear && d.State==State);
-		// console.log(trialData)
-
-
-		// let AgeData=this.data.Age.filter(d=>d.Year==activeYear && d.State==State);
-		// console.log("**************");
-		// console.log(AgeData)
-		
-		
-		// let barLayer=d3.select("#barLayer");
-
-		// let categories=[]
-		// for(let i=0;i<trialData.length;i++){
-		// 	if(!categories.includes(trialData[i].Categories))
-		// 	categories.push(trialData[i].Categories)
-		// }
-		// console.log(categories)
-
-		// var groups = ["Male", "Female"]
-
-
-
-
-
-		// var x = d3.scaleBand()
-		//       .domain(categories)
-		//       .range([0, width])
-		//       .padding([0.2]);
-	
-		// // barLayer.append("g")
-		// 	// .attr("transform","translate(0,300)")
-		//     // .call(d3.axisBottom(x).tickSize(0));
-			
-		// // let ticklabel = d3.selectAll(".tick").selectAll("text").attr("transform","rotate(90) translate(8,8)").style("text-anchor", "start");;
-		// // console.log(ticklabel);
-
-
-		//   // Add Y axis
-		//   var y = d3.scaleLinear()
-		//     .domain([-50, 2500])
-		//     .range([ 0, 300]);
-		//   // barLayer.append("g")
-		//     // .call(d3.axisLeft(y));
-
-		
-		//   var xSubgroup = d3.scaleBand()
-		//     .domain(groups)
-		//     .range([0, x.bandwidth()-26])
-		//     .padding([0.05])
-
-		  
-		//   var color = d3.scaleOrdinal()
-		//     .domain(groups)
-		//     .range(['#e41a1c','#377eb8'])
-			
-		//    barLayer.selectAll("rect")
-		// 			.data(trialData)
-		// 			.join("rect")
-		// 			  .attr("x", function(d,i) { return xSubgroup(d.Gender)+ x(d.Categories) ; })
-		// 			  .attr("y",function(d){ return 300 - y(d.Total)})
-		// 			  .attr("width", 20)
-		// 			  .attr("height",function(d){ return y(d.Total)})
-		// 			  .attr("fill", function(d) { return color(d.Gender); });
 
 
 		
